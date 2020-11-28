dbb76deb17f5dae38f755d8fcea2fa941a2f94dd
NotEXE <alekseipyanov@mail.ru>
2020-11-28 18:20:58 +0300

    fix movement animation

--- Assets/Scripts/Player/PlayerActions.cs
+++ Assets/Scripts/Player/PlayerActions.cs
@@ -11,46 +11,52 @@ public class PlayerActions
 
     public void Move(Transform transform)
     {
+        
         Vector2 newPosition = Vector2.MoveTowards(_player.Components.RigitBody.position, _player.Components.GridMovePoint.position, _player.Stats.Speed * Time.deltaTime);
-        _player.Components.RigitBody.MovePosition(newPosition);
 
+        _player.Components.RigitBody.MovePosition(newPosition);
+        
+        // .05f fix micro freezes every cell
         if (Vector2.Distance(_player.Components.RigitBody.position, _player.Components.GridMovePoint.position) <= .05f)
         {
+
             if (Mathf.Abs(_player.Stats.Direction.x) >= 1f)
             {
-                if(!Physics2D.OverlapCircle(_player.Components.GridMovePoint.position + new Vector3(_player.Stats.Direction.x, 0f, 0f), .2f, _player.Components.WhatStopMovement))
+                if (!Physics2D.OverlapCircle(_player.Components.GridMovePoint.position + new Vector3(_player.Stats.Direction.x, 0f, 0f), .2f, _player.Components.WhatStopMovement))
+                {
+                    //fix movement animation
+                    _player.Components.RigitBody.position = new Vector2(_player.Components.GridMovePoint.position.x, _player.Components.GridMovePoint.position.y);
+
                     _player.Components.GridMovePoint.position += new Vector3(_player.Stats.Direction.x, 0f, 0f);
+                    
+                }
             }
             else if (Mathf.Abs(_player.Stats.Direction.y) >= 1f)
             {
                 if (!Physics2D.OverlapCircle(_player.Components.GridMovePoint.position + new Vector3(0f, _player.Stats.Direction.y, 0f), .2f, _player.Components.WhatStopMovement))
+                {
+                    //fix movement animation
+                    _player.Components.RigitBody.position = new Vector2(_player.Components.GridMovePoint.position.x, _player.Components.GridMovePoint.position.y);
+
                     _player.Components.GridMovePoint.position += new Vector3(0f, _player.Stats.Direction.y, 0f);
+                }
             }
-            else 
+            else
             {
                 _player.Components.Animator.TryPlayAnimation("Idle");
-            }         
+            }
         }
-        else 
+        else
         {
             if (newPosition.x > _player.Components.RigitBody.position.x)
                 _player.Components.Animator.TryPlayAnimation("MoveRight");
-            else if(newPosition.x < _player.Components.RigitBody.position.x)
+            else if (newPosition.x < _player.Components.RigitBody.position.x)
                 _player.Components.Animator.TryPlayAnimation("MoveLeft");
-            else if (newPosition.y > _player.Components.RigitBody.position.y)
+            if (newPosition.y > _player.Components.RigitBody.position.y)
                 _player.Components.Animator.TryPlayAnimation("MoveUp");
             else if (newPosition.y < _player.Components.RigitBody.position.y)
-                _player.Components.Animator.TryPlayAnimation("MoveDown");
+                _player.Components.Animator.TryPlayAnimation("MoveDown"); 
         }
-        
-
-
-        //flip x
-        //if (_player.Stats.Direction.x != 0)
-        //{
-        //    transform.localScale = new Vector2(_player.Stats.Direction.x < 0 ? -1 : 1, 1);
-        //}
-
     }
 
     public void Attack()
